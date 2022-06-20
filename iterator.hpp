e0b9b169e0eb839e2efc05216d670b48d631d2c5
Gustave MARY <gmary@e3r3p10.clusters.42paris.fr>
2022-06-20 15:32:39 +0200

    reverse iterator seems to be done

--- iterator.hpp
+++ iterator.hpp
@@ -1,71 +1,105 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   iterator.hpp                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: gmary <gmary@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/17 11:17:13 by gmary             #+#    #+#             */
-/*   Updated: 2022/06/20 10:21:35 by gmary            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   iterator.hpp                                       :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: gmary <gmary@student.42.fr>                +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2022/06/17 11:17:13 by gmary             #+#    #+#             */
+// /*   Updated: 2022/06/20 10:58:38 by gmary            ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
 
-/*
-	Depuis c++11 tout constructeur sans explicit est un constructeur de conversion
-*/
+// /*
+// 	Depuis c++11 tout constructeur sans explicit est un constructeur de conversion
+// */
 
-#include "iterator_traits.hpp"
+// #include "iterator_traits.hpp"
 
 
-namespace ft {
+// namespace ft {
 
-	template <typename Iterator>
-	class random_access_iterator : public iterator_traits<Iterator>
-	{
-		protected:
-			Iterator						current;
-			typedef	ft::iterator_traits		traits_type;
-		public:
-			//TODO need to add some shit here
-			//!--------------------------------CONSTRUCTOR------------------------------------------------------
-			random_access_iterator() : current() {}
-			explicit random_access_iterator(Iterator it) : current(it) {} // [ ] pq avoir proteger le constructeur de tte conversion
-			random_access_iterator(const random_access_iterator<Iterator>& it) : current(it.current) {}
-			//!--------------------------------OPERATOR------------------------------------------------------
+// 	template <typename Iterator>
+// 	class random_access_iterator : public iterator_traits<Iterator>
+// 	{
+// 		protected:
+// 			Iterator								current;
+// 			typedef	ft::iterator_traits<Iterator>	traits_type;
+// 		public:
+// 			//TODO need to add some shit here
+// 			//!--------------------------------CONSTRUCTOR------------------------------------------------------
+// 			random_access_iterator() : current() {}
+// 			explicit random_access_iterator(Iterator it) : current(it) {} // [ ] pq avoir proteger le constructeur de tte conversion
+// 			random_access_iterator(const random_access_iterator<Iterator>& it) : current(it.current) {}
+// 			//!--------------------------------OPERATOR------------------------------------------------------
 
-			random_access_iterator& operator=(const random_access_iterator<Iterator>& it)
-			{
-				current = it.current;
-				return (*this);
-			}
+// 			random_access_iterator& operator=(const random_access_iterator<Iterator>& it)
+// 			{
+// 				// TODO: need to protect this
+// 				current = it.current;
+// 				return (*this);
+// 			}
 
-			random_access_iterator& operator++()
-			{
-				++current;
-				return (*this);
-			}
+// 			random_access_iterator& operator++()
+// 			{
+// 				++current;
+// 				return (*this);
+// 			}
 
-			random_access_iterator& operator--()
-			{
-				--current;
-				return (*this);
-			}
+// 			random_access_iterator& operator--()
+// 			{
+// 				--current;
+// 				return (*this);
+// 			}
 
-			random_access_iterator& operator+=(difference_type n)
-			{
-				current += n;
-				return (*this);
-			}
+// 			/*
+// 			difference type is equivalent of ptdrdiff_t, we now know that it is signed
+// 			and that it belongs to the same container
+// 			*/
+		
+// 			random_access_iterator& operator+=(difference_type n)
+// 			{
+// 				current += n;
+// 				return (*this);
+// 			}
 
-			random_access_iterator& operator-=(difference_type n)
-			{
-				current -= n;
-				return (*this);
-			}
+// 			random_access_iterator& operator-=(difference_type n)
+// 			{
+// 				current -= n;
+// 				return (*this);
+// 			}
 
-			//!--------------------------------FUNCTION------------------------------------------------------
-			//faire fucntion pour retourner l'iterator type ??
-			//operator
-	};
+// 			random_access_iterator operator+(difference_type n) const
+// 			{
+// 				return (random_access_iterator(current + n));
+// 			}
+
+// 			random_access_iterator operator-(difference_type n) const
+// 			{
+// 				return (random_access_iterator(current - n));
+// 			}
+			
+
+// 			/*
+// 				BUG to check 
+// 			*/
+// 			random_access_iterator operator+(const random_access_iterator<Iterator>& it) const
+// 			{
+// 				return (random_access_iterator(current + it.current));
+// 			}
+
+// 			random_access_iterator operator-(const random_access_iterator<Iterator>& it) const
+// 			{
+// 				return (random_access_iterator(current - it.current));
+// 			}
+
+// 			random_access_iterator operator==(const random_access_iterator<Iterator>& it) const
+// 			{
+// 				return (current == it.current);
+// 			}
+// 			//!--------------------------------FUNCTION------------------------------------------------------
+// 			//faire fucntion pour retourner l'iterator type ??
+// 			//operator
+// 	};
 	
-};
+// };
---
+++ reverse_itertor.cpp
@@ -0,0 +1,207 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_itertor.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmary <gmary@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/20 12:47:38 by gmary             #+#    #+#             */
+/*   Updated: 2022/06/20 15:31:54 by gmary            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REVERSE_ITERATOR_HPP
+# define REVERSE_ITERATOR_HPP
+# include "iterator_traits.hpp"
+
+/*
+	Les inline spécificateurs et __inline les spécificateurs indiquent au compilateur 
+	d’insérer une copie du corps de la fonction dans chaque endroit où la fonction est appelée.
+	le compilateur peut annuler une demande d'inline si on a des boucle, on a des var static, recursivite,
+	retourn different de void (peut etre aussi bool ), si il contient un switch ou goto, etc.
+	iniline permet l'acceleration de reduire la surchage des appelles de fonction
+*/
+
+namespace ft
+{
+	template <typename Iterator>
+	class reverse_iterator
+	{
+		protected:
+			Iterator current;
+			typedef reverse_iterator<Iterator>	traits_type;
+		public:
+			typedef Iterator												iterator_type;
+			typedef typename iterator_traits<Iterator>::difference_type		difference_type;
+			typedef typename iterator_traits<Iterator>::pointer				pointer;
+			typedef typename iterator_traits<Iterator>::reference			reference;
+			
+			//!----------------------------CONSTRUCTORS------------------------------------------------------
+			
+			reverse_iterator() : current() {}
+			
+			explicit reverse_iterator(iterator_type other) : current(other) {} //why need a explicit constructor?
+			
+			reverse_iterator(const reverse_iterator<Iterator>& other) : current(other.current) {}
+			
+			//!----------------------------OPERATORS---------------------------------------------------------
+			
+			
+			
+			//return c current, accesss the underlying container
+			base() const { return current; }
+
+			reverse_iterator& operator=(const reverse_iterator<Iterator>& other)
+			{
+				if (this != &other)
+				{
+					current = other.current;
+				}
+				return *this;
+			}
+			
+			//retourne une reference sur le contenu de l'itérateur
+			reference operator*() const 
+			{
+				Iterator tmp = current; 
+				return *(tmp);
+			}
+			
+			//retourne un pointeur sur le contenu de l'itérateur (a --current)
+			operator->() const { return &(operator*()); }
+
+			reverse_iterator & operator++()
+			{
+				--current;
+				return *this;
+			}
+			
+			reverse_iterator operator++(int)
+			{
+				reverse_iterator tmp = *this;
+				--current;
+				return (tmp);
+			}
+
+			reverse_iterator & operator--()
+			{
+				++current;
+				return *this;
+			}
+
+			reverse_iterator operator--(int)
+			{
+				reverse_iterator tmp = *this;
+				++current;
+				return (tmp);
+			}
+
+
+			// ici on utilise difference_type car on a sait que (ptrdiff_t) appartient forcement au container associee
+			reverse_iterator operator+(difference_type n) const
+			{
+				return (reverse_iterator(current - n));
+			}
+
+			reverse_iterator operator-(difference_type n) const
+			{
+				return (reverse_iterator(current + n));
+			}
+			
+			reverse_iterator & operator+=(difference_type n) // BUG peut etre cest pas ce quil faut faire
+			{
+				current -= n;
+				return *this;
+			}
+
+			reverse_iterator & operator-=(difference_type n) //BUG cets peut etre pas exactement ca
+			{
+				current += n;
+				return *this;
+			}
+			
+			reference operator[](difference_type n) const
+			{
+				return *(*this + n);
+			}
+	};
+	
+	//! template with one argument: const/const or non const/non const
+	
+	template <typename Iterator>
+	inline bool operator==(const reverse_iterator<Iterator> &lhs, const reverse_iterator<Iterator> &rhs)
+	{
+		return lhs.base() == rhs.base();
+	}
+	
+	template <typename Iterator>
+	inline bool operator!=(const reverse_iterator<Iterator> &lhs, const reverse_iterator<Iterator> &rhs)
+	{
+		return (lhs.base() != rhs.base());
+	}
+
+	template <typename Iterator>
+	inline bool operator<(const reverse_iterator<Iterator> &lhs, const reverse_iterator<Iterator> &rhs)
+	{
+		return lhs.base() < rhs.base();
+	}
+
+	template <typename Iterator>
+	inline bool operator>(const reverse_iterator<Iterator> &lhs, const reverse_iterator<Iterator> &rhs)
+	{
+		return lhs.base() > rhs.base();
+	}
+
+	template <typename Iterator>
+	inline bool operator<=(const reverse_iterator<Iterator> &lhs, const reverse_iterator<Iterator> &rhs)
+	{
+		return lhs.base() <= rhs.base();
+	}
+
+	template <typename Iterator>
+	inline bool operator>=(const reverse_iterator<Iterator> &lhs, const reverse_iterator<Iterator> &rhs)
+	{
+		return lhs.base() >= rhs.base();
+	}
+
+	//! template with two arguments: const/non const or non const/ const
+
+	template <typename IteratorL, typename IteratorR>
+	inline bool operator==(const reverse_iterator<IteratorL> &lhs, const reverse_iterator<IteratorR> &rhs)
+	{
+		return lhs.base() == rhs.base();
+	}
+	
+	template <typename IteratorL, typename IteratorR>
+	inline bool operator!=(const reverse_iterator<IteratorL> &lhs, const reverse_iterator<IteratorR> &rhs)
+	{
+		return lhs.base() != rhs.base();
+	}
+	
+	template <typename IteratorL, typename IteratorR>
+	inline bool operator<(const reverse_iterator<IteratorL> &lhs, const reverse_iterator<IteratorR> &rhs)
+	{
+		return lhs.base() < rhs.base();
+	}
+
+	template <typename IteratorL, typename IteratorR>
+	inline bool operator>(const reverse_iterator<IteratorL> &lhs, const reverse_iterator<IteratorR> &rhs)
+	{
+		return lhs.base() > rhs.base();
+	}
+
+	template <typename IteratorL, typename IteratorR>
+	inline bool operator<=(const reverse_iterator<IteratorL> &lhs, const reverse_iterator<IteratorR> &rhs)
+	{
+		return lhs.base() <= rhs.base();
+	}
+
+	template <typename IteratorL, typename IteratorR>
+	inline bool operator>=(const reverse_iterator<IteratorL> &lhs, const reverse_iterator<IteratorR> &rhs)
+	{
+		return lhs.base() >= rhs.base();
+	}
+	
+}
+
+#endif
