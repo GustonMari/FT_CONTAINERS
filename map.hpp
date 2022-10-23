73d23841fffefcfc83e4c5a950352f954285c6c2
gustave mary <gustave1mary@gmail.com>
2022-10-23 21:53:55 +0200

    todo list updated

---
+++ map.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmary <gmary@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/23 21:36:33 by gmary             #+#    #+#             */
+/*   Updated: 2022/10/23 21:44:26 by gmary            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAP_HPP
+#define MAP_HPP
+# include <memory>
+# include <vector>
+# include "iterator_traits.hpp"
+# include "reverse_iterator.hpp"
+# include "vector_base.hpp"
+# include "utils.hpp"
+# include "pair.hpp"
+
+namespace ft
+{
+	template <class Key, class T, class Compare = less<Key>, class Alloc = allocator<pair<const Key,T>> 
+	class map;
+	{
+		
+	}
+}
+
+#endif
