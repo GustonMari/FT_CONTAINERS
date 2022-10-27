10e43ef981187bab1e2b232b84ac7c54f91109e2
Gustave MARY <gmary@e3r6p8.clusters.42paris.fr>
2022-10-27 11:42:43 +0200

    stack in progress

---
+++ ft_container/stack.hpp
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmary <gmary@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/27 11:10:19 by gmary             #+#    #+#             */
+/*   Updated: 2022/10/27 11:41:04 by gmary            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STACK_HPP
+# define STACK_HPP
+
+#include "_colors.hpp"
+#include "utils.hpp"
+#include "vector.hpp"
+#include "enable_if.hpp"
+
+namespace ft
+{
+	template <class T, class Container = ft::vector<T> >
+	class stack
+	{
+		protected:
+			Container c;
+		
+		public:
+			typedef typename Container::value_type		value_type;
+			typedef typename Container::reference		reference;
+			typedef typename Container::const_reference	const_reference;
+			typedef typename Container::size_type		size_type;
+			typedef			 Container					container_type;
+			
+			explicit stack (const container_type& ctnr = container_type()) : c(ctnr) {}
+
+			bool empty() const { return c.empty(); }
+			
+			size_type size() const { return c.size(); }
+			
+			reference top() { return c.back(); }
+			
+			const_reference top() const { return c.back(); }
+			
+			void push (const value_type& val) { c.push_back(val); }
+			
+			void pop() { c.pop_back(); }
+	};
+
+template <class T, class Container>
+bool operator== (const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs)
+{
+	return (lhs.c == rhs.c);
+}
+
+template <class T, class Container>
+bool operator!= (const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs)
+{
+	return (lhs.c != rhs.c);
+}
+
+template <class T, class Container>
+bool operator<  (const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs)
+{
+	return (lhs.c < rhs.c);
+}
+
+template <class T, class Container>
+bool operator<= (const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs)
+{
+	return (lhs.c <= rhs.c);
+}
+
+template <class T, class Container>
+bool operator>  (const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs)
+{
+	return (lhs.c > rhs.c);
+}
+
+template <class T, class Container>
+bool operator>= (const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs)
+{
+	return (lhs.c >= rhs.c);
+}
+
+
+}
+
+#endif
