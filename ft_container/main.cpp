ce938d6c57f1b5d0f10dbe0d976e1c1cd1edc7b1
Gustave MARY <gmary@e3r5p9.clusters.42paris.fr>
2022-11-22 14:49:42 +0100

    empty test passed 2

--- ft_container/main.cpp
+++ ft_container/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gmary <gmary@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 11:33:21 by gmary             #+#    #+#             */
-/*   Updated: 2022/11/22 14:36:10 by gmary            ###   ########.fr       */
+/*   Updated: 2022/11/22 14:48:33 by gmary            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,8 +42,6 @@
 using namespace TESTED_NAMESPACE;
 
 
-using namespace TESTED_NAMESPACE;
-
 template <class Key, class T>
 void	print(map<Key, T>& lst)
 {
@@ -55,17 +53,34 @@ int main ()
 {
   map<char,int> mymap;
 
-  mymap['a']=10;
-  mymap['b']=20;
-  mymap['c']=30;
+  // first insert function version (single parameter):
+  mymap.insert ( pair<char,int>('a',100) );
+  mymap.insert ( pair<char,int>('z',200) );
 
-	for (map<char,int>::iterator it = mymap.begin(); it != mymap.end(); it++)
-		std::cout << it->first << " => " << it->second << " size: " << mymap.size() << '\n';
-  while (!mymap.empty())
-  {
-    // std::cout << mymap.begin()->first << " => " << mymap.begin()->second << '\n';
-    mymap.erase(mymap.begin());
+  pair<map<char,int>::iterator,bool> ret;
+  ret = mymap.insert ( pair<char,int>('z',500) );
+  if (ret.second==false) {
+    std::cout << "element 'z' already exists";
+    std::cout << " with a value of " << ret.first->second << '\n';
   }
+
+  // second insert function version (with hint position):
+  map<char,int>::iterator it = mymap.begin();
+  mymap.insert (it, pair<char,int>('b',300));  // max efficiency inserting
+  mymap.insert (it, pair<char,int>('c',400));  // no max efficiency inserting
+
+  // third insert function version (range insertion):
+  map<char,int> anothermap;
+  anothermap.insert(mymap.begin(),mymap.find('c'));
+
+  // showing contents:
+  std::cout << "mymap contains:\n";
+  for (it=mymap.begin(); it!=mymap.end(); ++it)
+    std::cout << it->first << " => " << it->second << '\n';
+
+  std::cout << "anothermap contains:\n";
+  for (it=anothermap.begin(); it!=anothermap.end(); ++it)
+    std::cout << it->first << " => " << it->second << '\n';
 
   return 0;
 }
